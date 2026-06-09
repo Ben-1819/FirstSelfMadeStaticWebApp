1ad1d6a7d845c8e85055d2fee13ddde63b5b49c8
Ben-1819 <ben.brown20332@gmail.com>
2026-06-09 16:13:56 +0100

    feat: Added whats next to the app

--- src/components/WhatsNext.vue
+++ src/components/WhatsNext.vue
@@ -2,4 +2,14 @@
 import { useRouter } from 'vue-router';
 </script>
 
-<template></template>
+<template>
+  <main>
+    <div>
+      <ul>
+        <li>Look at tutorials on putting up a php app</li>
+        <li>Learn how to connect the database of a laravel app to an azure database</li>
+        <li>Learn how to use Azure to deploy a vue frontend and a laravel backend</li>
+      </ul>
+    </div>
+  </main>
+</template>

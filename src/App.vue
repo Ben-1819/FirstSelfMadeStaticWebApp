3f3aa988e19e698d24f0a07ba9bc637461bac2df
Ben-1819 <ben.brown20332@gmail.com>
2026-06-09 15:34:29 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Button } from '@/components/ui/button';
+
+const show = ref<boolean>(false);
+
+const toggleShow = () => {
+  show.value = !show.value;
+};
+</script>
+
+<template>
+  <main>
+    <div class="flex flex-row items-center justify-center">
+      <Button variant="outline" @click="toggleShow">I'm Ben</Button>
+
+      <h1 v-if="show" class="text-2xl">Show is true!</h1>
+      <h1 v-else class="text-2xl">Show is false!</h1>
+    </div>
+  </main>
+</template>

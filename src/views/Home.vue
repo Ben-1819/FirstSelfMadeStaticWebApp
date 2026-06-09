c9a18c6e5241b517ab9d103c43ee63abf44e6d50
Ben-1819 <ben.brown20332@gmail.com>
2026-06-09 15:54:00 +0100

    feat: Added multiple components

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { Button } from '@/components/ui/button';
+import WhatsNext from '@/components/WhatsNext.vue';
+const router = useRouter();
+
+const goToSwitchyButton = () => {
+  router.push({
+    name: 'SwitchyButton',
+  });
+};
+</script>
+
+<template>
+  <main>
+    <div class="flex flex-col items-center">
+      <h1>This is the home page!</h1>
+      <div>
+        <h2>Whats next?</h2>
+        <div>
+          <WhatsNext />
+        </div>
+      </div>
+      <div>
+        <Button variant="outline" class="text-green-600" @click="goToSwitchyButton"
+          >Go to switchy button</Button
+        >
+      </div>
+    </div>
+  </main>
+</template>
---
+++ src/views/SwitchyButton.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+import { Button } from '@/components/ui/button';
+
+const router = useRouter();
+
+const show = ref<boolean>(false);
+
+const toggleShow = () => {
+  show.value = !show.value;
+};
+
+const goToHome = () => {
+  router.push({
+    name: 'home',
+  });
+};
+</script>
+
+<template>
+  <main>
+    <div class="flex flex-row items-center justify-center">
+      <Button variant="outline" @click="toggleShow">I'm Ben</Button>
+      <div>
+        <h1 v-if="show" class="text-2xl">Show is true!</h1>
+        <h1 v-else class="text-2xl">Show is false!</h1>
+      </div>
+      <div>
+        <Button variant="outline" class="text-green-600" @click="goToHome"
+          >Go to the home page</Button
+        >
+      </div>
+    </div>
+  </main>
+</template>
